a767ec8dba889466971604b4266fd5aa2ba7f7f5
Andreas Schwarz <69904660+drzott@users.noreply.github.com>
2020-08-26 12:21:47 +0200

    ADInfos

---
+++ C#/ReadADInfos/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.DirectoryServices.AccountManagement;
+using System.DirectoryServices;
+using System.IO;
+using System.Data.SqlClient;
+
+namespace ReadADInfos
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            DirectorySearcher se = new DirectorySearcher();
+            se.Filter = "(&(objectClass=group))";
+            se.SizeLimit = 0;
+            se.PageSize = 999;
+
+            // groups
+
+            SearchResultCollection results = se.FindAll();
+
+            List<string> groups = new List<string>();
+
+            foreach (SearchResult group in results)
+            {
+                var entry = new DirectoryEntry(group.Path);
+                string shortName = entry.Name.Substring(3, entry.Name.Length - 3);
+
+                if (!string.IsNullOrEmpty(shortName))
+                    groups.Add(shortName);
+            }
+
+            // users
+
+            se = new DirectorySearcher();
+
+            se.Filter = "(&(objectClass=user))";
+            se.SizeLimit = 0;
+            se.PageSize = 999;
+
+            results = null;         
+            results = se.FindAll();
+
+            List<User> users = new List<User>();
+
+            foreach (SearchResult user in results)
+            {
+                User u = new User(user);
+                users.Add(u);
+            }
+
+
+            if (ReadADInfos.Properties.Settings.Default.WriteFiles)
+            {
+
+                // Write Groups
+
+                using (StreamWriter w = new StreamWriter(Path.Combine(ReadADInfos.Properties.Settings.Default.ExportDir, "Groups.txt")))
+                {
+                    foreach (string s in groups)
+                    {
+                        w.WriteLine(s);
+                    }
+
+                    w.Close();
+                }
+
+                // Write Users
+
+                using (StreamWriter w = new StreamWriter(Path.Combine(ReadADInfos.Properties.Settings.Default.ExportDir, "Users.txt")))
+                {
+                    foreach (User u in users)
+                    {
+                        w.WriteLine(u.SamAccountName);
+                        w.WriteLine(u.Firstname);
+                        w.WriteLine(u.Lastname);
+                        w.WriteLine(u.EMail);
+                        w.WriteLine(u.Department);
+                        w.WriteLine(u.UAC);
+                        w.WriteLine(u.Wildcard);
+                        w.WriteLine();
+
+                        foreach (string g in u.Groups)
+                        {
+                            w.WriteLine(g);
+                        }
+
+                        w.WriteLine();
+
+                        foreach (string p in u.AvailableProperties)
+                        {
+                            w.WriteLine(p);
+                        }
+
+                        w.WriteLine();
+                        w.WriteLine();
+                        w.WriteLine();
+
+                    }
+
+                    w.Close();
+                }
+            }
+
+
+            if (ReadADInfos.Properties.Settings.Default.PersistToDatabase)
+            {
+                CheckDatabase();
+
+                using (SqlConnection conn = new SqlConnection(ReadADInfos.Properties.Settings.Default.ConnectionString))
+                {
+                    string sql = "INSERT INTO AD_Import (SamAccountName, Firstname, Lastname, EMail, Department, UAC) Values (@s, @f, @l, @e,@d, @u)";
+
+                    conn.Open();
+
+                    foreach (User u in users)
+                    {
+                        if (string.IsNullOrEmpty(u.SamAccountName))
+                            continue;
+
+                        if (ReadADInfos.Properties.Settings.Default.PersistIfEmailAvailable && string.IsNullOrEmpty(u.EMail))
+                            continue;
+
+                        if (ReadADInfos.Properties.Settings.Default.PersistIfDepartmentAvailable && string.IsNullOrEmpty(u.Department))
+                            continue;
+
+                        SqlCommand cmd = new SqlCommand(sql, conn);
+
+                        cmd.Parameters.AddWithValue("@s", u.SamAccountName);
+                        cmd.Parameters.AddWithValue("@f", u.Firstname);
+                        cmd.Parameters.AddWithValue("@l", u.Lastname);
+                        cmd.Parameters.AddWithValue("@e", u.EMail);
+                        cmd.Parameters.AddWithValue("@d", u.Department);
+                        cmd.Parameters.AddWithValue("@u", u.UAC);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+
+        public static void CheckDatabase()
+        {
+            using (SqlConnection conn = new SqlConnection(ReadADInfos.Properties.Settings.Default.ConnectionString))
+            {
+                string sql = @"IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'AD_Import')
+                                BEGIN
+
+
+		                                CREATE TABLE AD_Import(
+			                                [Id] [bigint] IDENTITY(1,1) NOT NULL,
+			                                [SamAccountname] [varchar](250) NULL,
+			                                [FirstName] [varchar](250) NULL,
+			                                [LastName] [varchar](250) NULL,
+			                                [EMail] [varchar](250) NULL,
+			                                [Department] [varchar](250) NULL,
+			                                [UAC] [bigint] NULL,
+		                                 CONSTRAINT [PK_AD_Import] PRIMARY KEY CLUSTERED 
+		                                (
+			                                [Id] ASC
+		                                )
+		                                )
+
+                                END
+
+                                TRUNCATE TABLE AD_Import
+                                ";
+
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+    }
+}
---
+++ C#/ReadADInfos/User.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.DirectoryServices;
+
+namespace ReadADInfos
+{
+    public class User
+    {
+        SearchResult _sr = null;
+
+        public List<string> AvailableProperties = new List<string>();
+
+        public User(SearchResult s)
+        {
+            if (s == null)
+                return;
+
+            _sr = s;
+
+            if(s.Properties != null && s.Properties.Count > 0 && s.Properties.PropertyNames != null && s.Properties.PropertyNames.Count > 0)
+            {
+                foreach(string p in _sr.Properties.PropertyNames)
+                {
+                    AvailableProperties.Add(p);
+                }
+            }
+        }
+
+        public string Wildcard
+        {
+
+
+            get
+            {
+                if (AvailableProperties.Contains(ReadADInfos.Properties.Settings.Default.Wildcard))
+                {
+                    if (_sr.Properties[ReadADInfos.Properties.Settings.Default.Wildcard] != null && _sr.Properties[ReadADInfos.Properties.Settings.Default.Wildcard].Count > 0)
+                        return _sr.Properties[ReadADInfos.Properties.Settings.Default.Wildcard][0].ToString();
+                }
+
+                return string.Empty;
+            }
+        }
+
+
+        public int UAC
+        {
+
+            //  512 = normal account
+            //  514 = 512 + 2 = normal account, disabled
+            //  546 = 512 + 32 + 2 = normal account, disabled, no password required
+            // 2080 = 2048 + 32 = Interdomain trust, no password required
+            //66048 = 65536 + 512 = normal account. password never expires
+            //66050 = 65536 + 512 + 2 = normal account. password never expires, disabled
+            //66080 = 65536 + 512 + 32 = normal account. password never expires, no password required
+
+            get
+            {
+                if (AvailableProperties.Contains("useraccountcontrol"))
+                {
+                    if (_sr.Properties["useraccountcontrol"] != null && _sr.Properties["useraccountcontrol"].Count > 0)
+                        return Convert.ToInt32(_sr.Properties["useraccountcontrol"][0]);
+                }
+
+                return 0;
+            }
+        }
+
+        public string EMail
+        {
+            get
+            {
+                if (AvailableProperties.Contains("mail"))
+                {
+                    if(_sr.Properties["mail"] != null && _sr.Properties["mail"].Count > 0)
+                        return _sr.Properties["mail"][0].ToString();
+                }
+
+                return string.Empty;
+            }
+        }
+
+        public string Department
+        {
+            get
+            {
+                if (AvailableProperties.Contains("department"))
+                {
+                    if (_sr.Properties["department"] != null && _sr.Properties["department"].Count > 0)
+                        return _sr.Properties["department"][0].ToString();
+                }
+
+                return string.Empty;
+            }
+        }
+
+
+        public string Firstname
+        {
+            get
+            {
+                if (AvailableProperties.Contains("givenname"))
+                {
+                    if (_sr.Properties["givenname"] != null && _sr.Properties["givenname"].Count > 0)
+                        return _sr.Properties["givenname"][0].ToString();
+                }
+
+                return string.Empty;
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                if (AvailableProperties.Contains("name"))
+                {
+                    if (_sr.Properties["name"] != null && _sr.Properties["name"].Count > 0 && _sr.Properties["givenname"] != null && _sr.Properties["givenname"].Count > 0)
+                    {
+                        string str = _sr.Properties["givenname"][0].ToString();
+                        string str2 = _sr.Properties["name"][0].ToString();
+
+                        return (str2.Replace(str, "")).Trim();
+                    }
+                }
+
+                return string.Empty;
+            }
+        }
+
+
+
+        public string SamAccountName
+        {
+            get
+            {
+                if (AvailableProperties.Contains("samaccountname"))
+                {
+                    if (_sr.Properties["samaccountname"] != null && _sr.Properties["samaccountname"].Count > 0)
+                        return _sr.Properties["samaccountname"][0].ToString();
+                }
+
+                return string.Empty;
+            }
+        }
+
+
+        public List<string> Groups
+        {
+            get
+            {
+                if (AvailableProperties.Contains("memberof"))
+                {
+                    if (_sr.Properties["memberof"] != null)
+                    {
+                        List<string> r = new List<string>();
+                        foreach(string s in _sr.Properties["memberof"])
+                        {
+                            r.Add(s.Substring(3, s.Length - 3));
+                        }
+
+                        return r;
+                    }
+                        
+                }
+
+                return new List<string>();
+            }
+        }
+
+
+    }
+}
